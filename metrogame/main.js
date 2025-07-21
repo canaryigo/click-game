743f5623c16c24d708bcfd86454822f6c0348733
canaryigo <saidaiasuka@gmail.com>
2025-07-21 22:41:41 +0900

    投了ボタンを修正3

--- metrogame/main.js
+++ metrogame/main.js
@@ -1,5 +1,5 @@
 import { drawMap } from "./map.js";
-import { drawCharacters, enableStationClicks } from "./ui.js";
+import { drawCharacters, enableStationClicks, resetGame } from "./ui.js";
 import { gameState } from "./state.js";
 
 window.addEventListener("DOMContentLoaded", () => {
